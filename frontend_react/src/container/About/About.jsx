25e605be03884cfccf09c9719439c7b065bd9417
malpakw <marcin.zietara.kw@gmail.com>
2022-03-20 10:00:13 +0100

    work section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import { images } from  '../../constants';
+import { AppWrap } from '../../wrapper';
+
 import './About.scss';
 import { urlFor, client } from '../../client'
 
@@ -42,4 +44,4 @@ const About = () => {
   )
 }
 
-export default About
+export default AppWrap(About, 'about');
--- frontend_react/src/wrapper/AppWrap.js
+++ frontend_react/src/wrapper/AppWrap.js
@@ -6,8 +6,7 @@ const AppWrap = (Component, idName, classNames) => function HOC() {
     <div id={idName} className={`app__container ${classNames}`}>
       <SocialMedia />
       <div className="app__wrapper app__flex">
-        <Component />
-
+      <Component />
         <div className="copyright">
           <p className="p-text">@2020 MICHAEL</p>
           <p className="p-text">All rights reserved</p>
